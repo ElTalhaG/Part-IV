046b84d03773855e99676c2e0e7f3b1ca607bb2e
ElTalhaG <epictalha1@gmail.com>
2026-04-17 11:02:31 +0200

    Link Player entries back to User with JPA relation

--- src/main/java/com/example/accessing_data_rest/model/User.java
+++ src/main/java/com/example/accessing_data_rest/model/User.java
@@ -18,6 +18,10 @@ public class User {
     // TODO this class needs to be extended with references to Player and
     //      the other way round (similar to the reference from Game to Player
     //      and the other way round.
+    // here we keep the list of player entries that belong to this user
+    @OneToMany(mappedBy = "user")
+    // here we store all the player objects for the user in one place
+    private List<Player> players;
 
     public long getUid() {
         return uid;
@@ -33,6 +37,16 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    // here we return the list so the backend can read which games the user joined
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    // here we save a whole list of player entries on the user
+    public void setPlayers(List<Player> players) {
+        this.players = players;
     }
 
 }
